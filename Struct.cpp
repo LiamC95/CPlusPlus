086884ac6214df7b6646228acaba6a93b9dca9aa
D00156796 <D00156796@student.dkit.ie>
2019-10-07 12:15:03 +0100

    Update Struct.cpp

--- Struct.cpp
+++ Struct.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 
 using namespace std;
+
+//? 2.	Write a program that declares a variable of type fruitType, prompts the user to input data about a fruit, and outputs the fruit data.
 struct fruitType
 {
     string Fruitname;
@@ -40,6 +42,18 @@ void displayFruit(fruitType fruit)
     cout << "Carbs\t:"<< fruit.carbs << endl;
     cout << "Sugars\t:"<< fruit.sugar << endl;
 }
+
+//? 3.	Write a program that reads students’ names followed by their test scores. The program should output each student’s name followed by the test scores and the relevant grade. It should also find and print the highest test score and the name of the students having the highest test score.
+struct student
+{
+    string  name;
+    int     caGrade;
+    int     testGrade;
+    float   average;
+};
+
+
+
 
 int main()
 {
